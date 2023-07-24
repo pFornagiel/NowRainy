a49b791f188e6fca978dc0a621b570e29515180d
Massny <pawel.fornagiel2@gmail.com>
2023-07-24 20:04:32 +0200

    changed vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import svgr from "vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig({
   server:{
+    host: '0.0.0.0',
     proxy:{
       '/api': 'http://localhosT:8080'
     }

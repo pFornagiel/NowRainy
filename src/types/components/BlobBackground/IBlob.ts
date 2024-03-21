7efbe72cf1fb9a8df7cb171fc014d880f8ce1bd9
Massny <pawel.fornagiel2@gmail.com>
2024-03-21 09:42:54 +0100

    separate type declaration to distinct directory

---
+++ src/types/components/BlobBackground/IBlob.ts
@@ -0,0 +1,14 @@
+import Perlin from "../../../types/utils/perlin3d"
+
+type MeshObject = THREE.Mesh<THREE.SphereGeometry, THREE.ShaderMaterial>
+
+interface objectTemplate {
+  object: MeshObject,
+  noise: Perlin,
+}
+
+interface BlobBackgroundProps {
+  children: JSX.Element | JSX.Element[],
+}
+
+export {BlobBackgroundProps, objectTemplate, MeshObject}
